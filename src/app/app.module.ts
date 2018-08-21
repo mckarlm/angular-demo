388c791eeb0f78bf075c61b5cc3ae0eeef57003c
Mckarl Manuel <manmckarl@gmail.com>
2018-08-21 10:13:41 -1000

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'; // for routing
 
 import { AppComponent } from './app.component';
+
+//pages
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component'
+import { ContactComponent } from './pages/contact/contact.component';
+
+//shared components
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //duuuude
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
